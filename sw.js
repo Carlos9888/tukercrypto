0c6536adb66c7157a9baf30859b3f243735fc5bd
Carlos9888 <miyakojunior@gmail.com>
2025-10-06 00:23:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
+const CACHE_NAME = "tukercrypto-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "https://www.tukercrypto.com/assets/tc.png",
+  "https://www.tukercrypto.com/assets/tc.png",
+  "/style.css",
+  "/script.js"
+];
+
+// Install SW
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("tukercrypto-cache").then((cache) => {
-      return cache.addAll([
-        "/index.html",
-        "/assets/logo-192.png",
-        "/assets/logo-512.png"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Activate SW (hapus cache lama)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch request
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
